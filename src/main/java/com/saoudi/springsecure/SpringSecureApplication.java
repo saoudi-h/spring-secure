594caa125999f0a1e26bd2ddd6de3585f2570040
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-21 00:00:10 +0200

    enableConfigurationProperties and add an init method

--- src/main/java/com/saoudi/springsecure/SpringSecureApplication.java
+++ src/main/java/com/saoudi/springsecure/SpringSecureApplication.java
@@ -1,15 +1,29 @@
 package com.saoudi.springsecure;
 
+import com.saoudi.springsecure.storage.StorageProperties;
+import com.saoudi.springsecure.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringSecureApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecureApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 
 }

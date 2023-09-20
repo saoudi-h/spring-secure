a1de9f99146e0806bec21368919bff2bbb77a087
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-20 18:11:05 +0200

    first commit

---
+++ src/test/java/com/saoudi/springsecure/SpringSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saoudi.springsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
